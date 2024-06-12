796f6c5d3a839d865a8de7f173868a39a96eca1f
sebprt <sebastien@kiboko.fr>
2024-06-12 15:50:54 +0200

    Initial commit

---
+++ jane/open_api.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'openapi-file' => __DIR__.'/docs.json',
+    'namespace' => 'Cegid\Api',
+    'directory' => __DIR__ . '/../src/',
+    'strict' => false,
+    'use-fixer' => true,
+    'throw-unexpected-status-code' => true,
+];
